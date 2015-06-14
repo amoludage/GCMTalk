30c51b7ecbee0b8177e6b1af184fd89fcc737e61
Amol <amoludage@joshsoftware.com>
2015-06-14 14:11:22 +0530

    notification

--- script.rb
+++ script.rb
@@ -5,12 +5,11 @@ class GcmApp
   def sendMessage(options)
     gcm = GCM.new(GCM_API_KEY) #GCM_API_KEY is declared in config/initializers/api_key.rb
     response = gcm.send_notification(REGISTRATION_ID,options) #REGISTRATION_ID also in initializers
-    p REGISTRATION_ID
-    puts response
+    puts response.first
   end
 
 end
 
-options = {data: {message: "Hello this is demo for GCM..Thanks"}}
+options = {data: {message: "Hello this is demo for GCM notification..Thanks"}}
 app = GcmApp.new
 app.sendMessage(options)
